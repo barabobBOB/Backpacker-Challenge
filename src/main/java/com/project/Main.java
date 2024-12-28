8d5cff358bb5136c1e847db3139b347404b606b3
choiseyeon <choi20014830@gmail.com>
2024-12-28 20:29:09 +0900

    feat: spark 세팅

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,7 +1,22 @@
 package com.project;
 
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.TimeZone;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SparkSession spark = SparkSession.builder()
+                .appName("ECommerceActivityLog")
+                .master("local[*]") // 로컬 모드에서 실행
+                .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
+                .config("spark.sql.parquet.compression.codec", "snappy")
+                .enableHiveSupport()
+                .getOrCreate();
+
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("SparkSession initialized successfully!");
     }
 }
