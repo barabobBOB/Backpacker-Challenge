14fd652771e219439a874f73dc8a5a714d7c6703
choiseyeon <choi20014830@gmail.com>
2024-12-28 20:56:32 +0900

    feat: KST 기준 일별 데일리 파티션 처리합니다.

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,8 +1,6 @@
 package com.project;
 
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.*;
 
 import java.util.TimeZone;
 
@@ -10,13 +8,26 @@ public class Main {
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder()
                 .appName("ECommerceActivityLog")
-                .master("local[*]") // 로컬 모드에서 실행
+                .master("local[*]")
                 .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
                 .config("spark.sql.parquet.compression.codec", "snappy")
                 .enableHiveSupport()
                 .getOrCreate();
-
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-        System.out.println("SparkSession initialized successfully!");
+
+        Dataset<Row> rawData = spark.read()
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .csv("./src/main/java/com/project/data/input/2019-*.csv");
+
+        Dataset<Row> processedData = rawData
+                .withColumn("event_time_kst", functions.from_utc_timestamp(functions.col("event_time"), "Asia/Seoul"))
+                .withColumn("partition_date", functions.date_format(functions.col("event_time_kst"), "yyyy-MM-dd"));
+
+        processedData.write()
+                .mode(SaveMode.Overwrite)
+                .partitionBy("partition_date")
+                .parquet("./src/main/java/com/project/data/output/parquet/");
+
     }
 }
